f98fe8383b43b907bd65da94970126be9e875d14
unknown <adon.moskal@gmail.com>
2023-10-02 11:42:54 +1300

    First commit

---
+++ src/routes/question1/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+
+    /*
+
+    Assemble the Avengers! (literally...)
+
+        We have 2 data files at 2 different URLs - 1 has first names, and the 2nd has 
+        last names.
+        You need to assemble the full names of these Avengers and print them out in
+        the ordered list below. (See PDF for final result.)
+
+        The first names can be found at: https://gist.githubusercontent.com/dfenders/0fe9021b4242d83d973ac7087287e166/raw/8c0bda392b0381cfd254b719360e177fdf98b1ab/firstNames.json
+        The last names can be found at: https://gist.githubusercontent.com/dfenders/0fe9021b4242d83d973ac7087287e166/raw/8c0bda392b0381cfd254b719360e177fdf98b1ab/lastNames.json
+
+    */
+
+    let avengers = [];
+
+</script>
+
+Refer to the code for the instructions.
+
+<ol>
+
+</ol>
